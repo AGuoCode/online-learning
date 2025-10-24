afc698330dc5001b4bc8eae48cb24d79b8ac8c2b
AGuoCode <aguo.coding@gmail.com>
2025-10-24 20:01:37 +0700

    add example request for controller

--- src/main/java/com/guai/onlinelearning/auth/AuthController.java
+++ src/main/java/com/guai/onlinelearning/auth/AuthController.java
@@ -4,6 +4,7 @@ import com.guai.onlinelearning.common.OperationResponse;
 import com.guai.onlinelearning.exception.ErrorResponse;
 import com.guai.onlinelearning.exception.StatusCode;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -25,6 +26,18 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Sample Update Admin Information",
+            content = @Content(
+                    examples = @ExampleObject(name = "Example Update Admin Request",
+                            value = """
+                                       {
+                                         "username": "johndoe@gmai.com",
+                                         "password": "Your password"
+                                       }
+                                    """
+                    )
+            )
+    )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Login success",
                     content = @Content(schema = @Schema(implementation = OperationResponse.class))),
@@ -40,6 +53,7 @@ public class AuthController {
                 OperationResponse.buildSuccessResponse(
                         StatusCode.SUCCESS,
                         "Login success",
+                        null,
                         authService.login(authRequest),
                         null)
         );
--- src/main/java/com/guai/onlinelearning/userDomain/admin/AdminController.java
+++ src/main/java/com/guai/onlinelearning/userDomain/admin/AdminController.java
@@ -5,6 +5,7 @@ import com.guai.onlinelearning.exception.ErrorResponse;
 import com.guai.onlinelearning.exception.StatusCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -24,6 +25,25 @@ public class AdminController {
 
     @PutMapping("/{id}")
     @Operation(summary = "Update Admin")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Sample Update Admin Information",
+            content = @Content(
+                    examples = @ExampleObject(name = "Example Update Admin Request",
+                            value = """
+                                       {
+                                           "name": "Your youtube name or school name",
+                                           "email": "example@gmail.com",
+                                           "article": "Your article",
+                                           "professional": "Your Professional",
+                                           "bio": "Your Bio",
+                                           "certificates": [
+                                             "Your Certificate List"
+                                           ],
+                                           "role": "ADMIN"
+                                        }
+                                    """
+                    )
+            )
+    )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Update admin success",
                     content = @Content(schema = @Schema(implementation = OperationResponse.class))),
@@ -44,6 +64,7 @@ public class AdminController {
                                 StatusCode.SUCCESS,
                                 "Update admin success",
                                 adminService.update(id, request),
+                                null,
                                 null)
                 );
     }
@@ -56,8 +77,6 @@ public class AdminController {
             @ApiResponse(responseCode = "400", description = "Retrieved admin failed",
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "405", description = "Invalid method input",
-                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "417", description = "Validation failed",
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     public ResponseEntity<OperationResponse> getAdminById(@PathVariable Integer id) {
@@ -67,6 +86,7 @@ public class AdminController {
                         OperationResponse.buildSuccessResponse(
                                 StatusCode.SUCCESS,
                                 "Retrieved admin success",
+                                null,
                                 adminService.findById(id),
                                 null)
                 );
