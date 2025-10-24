43190304f6e2570797fa4e419b5d1266bafc8565
AGuoCode <aguo.coding@gmail.com>
2025-10-24 20:20:56 +0700

    feature: generate OTP

---
+++ src/main/java/com/guai/onlinelearning/utilities/GenerateOTP.java
@@ -0,0 +1,19 @@
+package com.guai.onlinelearning.utilities;
+
+public class GenerateOTP {
+
+    public static String generateOTPCode(int length) {
+        // TODO I will currently use default otpcode before implemnt Email Service
+        return "123456";
+//        final String otpCharacter = "0123456789";
+//        final StringBuilder otpBuilder = new StringBuilder();
+//
+//        SecureRandom random = new SecureRandom();
+//
+//        for (int i = 0; i < length; i++) {
+//            int randomIndex = random.nextInt(otpCharacter.length());
+//            otpBuilder.append(otpCharacter.charAt(randomIndex));
+//        }
+//        return otpBuilder.toString();
+    }
+}
